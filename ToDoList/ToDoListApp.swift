81184b8ba5323ef397fcdcb84bc95ce42c31e22a
Randy Setiawan Hoesin <randy.setiawan@bumn.go.id>
2024-04-24 11:01:18 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Randy on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
